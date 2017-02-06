349708344428bca5025ed9d8fb7ed9d4d1919f4b
liule1988 <778252899@qq.com>
2017-02-06 17:25:53 +0800

    搭建好环境

---
+++ src/entry.js
@@ -0,0 +1,12 @@
+/**
+ * Created by leliu5 on 2017/2/6.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render((
+    <div>
+        dsdsa
+    </div>
+), document.getElementById("app"));
